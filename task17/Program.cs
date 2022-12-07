a69aa15b333998dd4187178d48428887af192b10
naumovanatalia <naumova.na43@mail.ru>
2022-12-07 20:43:24 +0200

    add task17

---
+++ task17/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите координату точки X");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Y");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1 четверть");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("2 четверть");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("3 четверть");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("4 четверть");
+}
