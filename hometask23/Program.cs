a6449e207de71838c25043734ae6eebd5a055eae
naumovanatalia <naumova.na43@mail.ru>
2022-12-07 22:33:02 +0200

    add hometask23

---
+++ hometask23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 1)
+{
+    Console.WriteLine("Введите число больше 0");
+}
+for(int i = 1; i <= a; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}, ");
+}
