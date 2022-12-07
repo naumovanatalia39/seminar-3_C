f390f351b60bdeb0f9f1bd6a7cb268332d3754c9
naumovanatalia <naumova.na43@mail.ru>
2022-12-07 21:09:13 +0200

    add task18

---
+++ task18/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите номер координатной четверти");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 4 || a < 0)
+{
+    Console.WriteLine("Нет такой четверти");
+}
+else if (a == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (a == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (a == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (a == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
