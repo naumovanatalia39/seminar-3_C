b26d2fce4eb68e2ffd131cf0073479fde0e12b5d
naumovanatalia <naumova.na43@mail.ru>
2022-12-07 22:24:18 +0200

    add hometask21

---
+++ hometask21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение x первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение z первой точки");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение x второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение z второй точки");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между точками = {res:f2}");
