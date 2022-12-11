b0f33ca5627e90f42677a59ab98bd491b45fd4cb
naumovanatalia <naumova.na43@mail.ru>
2022-12-11 11:26:46 +0200

    add hometask19

---
+++ hometask19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 10000 || a > 99999)
+{
+    Console.WriteLine("Число не пятизначное");
+}
+else if (a == (a % 10) * 10000 + ((a / 10) % 10) * 1000 + 
+((a / 100) % 10) * 100 + ((a / 1000) % 10) * 10 + ((a / 10000)))
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
